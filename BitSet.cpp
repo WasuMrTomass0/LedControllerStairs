2965e773032ffeb990adc7aba34b9ba96326a78f
Tomasz Moleda <tomasz_moleda@wp.pl>
2020-08-09 21:22:56 +0200

    Resign from BitSet

--- BitSet.cpp
+++ BitSet.cpp
@@ -1,12 +1,12 @@
-#include "BitSet.h"
-
-const unsigned WORD_SIZE = sizeof(word_t) * 8;
-
-
-BitSet::BitSet() {
-	data = static_cast<word_t*>(malloc((WORD_SIZE - 1) / 32 + 1));
-}
-
-BitSet::~BitSet() {
-	delete data;
-}
+//#include "BitSet.h"
+//
+//const unsigned WORD_SIZE = sizeof(word_t) * 8;
+//
+//
+//BitSet::BitSet() {
+//	data = static_cast<word_t*>(malloc((WORD_SIZE - 1) / 32 + 1));
+//}
+//
+//BitSet::~BitSet() {
+//	delete data;
+//}
