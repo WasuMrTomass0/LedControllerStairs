f763aaa3a7cd906f1b8bea50149dfbb79d785f32
WasuPC <pan.t@op.pl>
2021-01-22 19:20:40 +0100

    Delete NotCopyable.h

--- MovementSensorController/NotCopyable.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class NotCopyable {
-private:
-    // NotCopyable();
-public:
-    //NotCopyable(const NotCopyable&) = delete; // non construction-copyable
-    //NotCopyable& operator=(const NotCopyable&) = delete; // non copyable
-};
